3d507c2007b313a657f8dc207d5b3bd6d2c253f4
SwapnilRedekar <redekarswapnil010@gmail.com>
2020-10-02 20:00:00 +0530

    Added: spinner functionality for specified fields of Donor

--- app/src/main/java/project/dscjss/plasmadonor/Fragment/PatientFormFragment.kt
+++ app/src/main/java/project/dscjss/plasmadonor/Fragment/PatientFormFragment.kt
@@ -1,6 +1,5 @@
 package project.dscjss.plasmadonor.Fragment
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -74,6 +73,8 @@ class PatientFormFragment : Fragment() {
         }
 
     }
+
+
 
     private fun insertData() {
         var PatientDetails = HashMap<String, String>()
