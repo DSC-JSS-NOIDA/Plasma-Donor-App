75cbbdd12cdfe489f17b19b55a05b60fddf75a68
SanushRadalage <maleekasanush@gmail.com>
2020-10-02 06:23:48 +0530

    fixed https://github.com/DSC-JSS-NOIDA/Plasma-Donor-App/issues/32

--- app/src/main/java/project/dscjss/plasmadonor/Activity/SplashActivity.kt
+++ app/src/main/java/project/dscjss/plasmadonor/Activity/SplashActivity.kt
@@ -3,8 +3,10 @@ package project.dscjss.plasmadonor.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 import project.dscjss.plasmadonor.R
 
 class SplashActivity : AppCompatActivity() {
@@ -19,9 +21,11 @@ class SplashActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             if (firebaseAuth.currentUser != null) {
+                splashIcon.visibility = View.GONE
                 startActivity(Intent(applicationContext , MainActivity::class.java))
             }
             else {
+               splashIcon.visibility = View.GONE
                 startActivity(Intent(applicationContext, UserLoginActivity::class.java))
             }
             finish()
